e097eb628dafeb944cc7a7a09656d714a3b542f3
Jack Lyons <jack.edward.lyons@gmail.com>
2023-01-04 14:25:04 +1000

    Feat: Add pages to app

---
+++ pages/hello.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <h2>Hello page</h2>
+    <pre>error:  {{ error }}</pre>
+    <pre>pending:  {{ pending }}</pre>
+
+    <pre>{{ data }}</pre>
+    <NuxtWelcome />
+  </div>
+</template>
+<script setup>
+const route = useRoute();
+
+const { data, pending, error } = await useAsyncData("hello api", (context) => {
+  const index = route.query.index;
+
+  return $fetch("/api/hello/" + index);
+});
+</script>
