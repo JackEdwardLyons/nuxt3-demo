1021373e8568d02b5689123644b1e71d4f9d1b82
Jack Lyons <jack.edward.lyons@gmail.com>
2023-01-04 11:20:37 +1000

    Chore: update to test useAsyncData

--- app.vue
+++ app.vue
@@ -1,9 +1,14 @@
 <template>
   <div>
+    <pre>error:  {{ error }}</pre>
+    <pre>pending:  {{ pending }}</pre>
+    <pre>refresh:  {{ refresh }}</pre>
     <pre>{{ data }}</pre>
     <NuxtWelcome />
   </div>
 </template>
 <script setup>
-const { data } = useFetch("/api/hello");
+const { data, pending, error, refresh } = useAsyncData("hello api", () =>
+  $fetch("/api/hello")
+);
 </script>
